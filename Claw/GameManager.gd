77eac451fe967f2bc915d7d6e4ce7f78f016e2ed
Magnus Larsen <golgothasTerror101@gmail.com>
2023-02-04 14:28:27 -0800

    Fix turnip spawning

--- Claw/GameManager.gd
+++ Claw/GameManager.gd
@@ -1,5 +1,4 @@
-extends Node
-
+extends Node2D
 class_name GameManager
 
 
@@ -7,9 +6,10 @@ class_name GameManager
 # var a = 2
 # var b = "text"
 
-export(PackedScene) var turnip
+export(PackedScene) var turnip = load("res://Claw/Turnip.tscn")
 export(Array, PackedScene) var turnips = [];
-export var max_turnips : int
+export var max_turnips : int = 10
+export var spawn_radius: Vector2 = Vector2(300, 200)
 
 
 # Called when the node enters the scene tree for the first time.
@@ -23,7 +23,6 @@ func _process(delta):
 	if(turnips.size() < max_turnips):
 		create_turnip()
 
-	pass
 
 func create_turnip():
 
@@ -32,14 +31,10 @@ func create_turnip():
 	add_child(new_turnip)
 
 	new_turnip.set_owner(self)
-	new_turnip.set_position(get_random_position())
+	new_turnip.global_position = get_random_position()
 
-	pass
 
 func get_random_position():
-	
-	# to do
-
-	return Vector2(0.0, 0.0)
-
-	pass
+	var pos = self.global_position + spawn_radius * Vector2(randf() * 2.0 - 1.0, randf() * 2.0 - 1.0).normalized()
+	print(pos)
+	return pos
