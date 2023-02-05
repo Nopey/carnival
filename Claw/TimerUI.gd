aa06b40351a55336d3da9f2b9e0452c4013c80bd
NolanMeske <nolanmeske@icloud.com>
2023-02-05 14:35:26 -0800

    ui is now above things

--- Claw/TimerUI.gd
+++ Claw/TimerUI.gd
@@ -1,6 +1,6 @@
 extends Label
 
-onready var timer = get_parent().get_node("Timer")
+onready var timer = get_parent().get_parent().get_node("Timer")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
