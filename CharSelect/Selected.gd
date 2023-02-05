fb6fc2f0b31f4733c008900e1873b3a4b15a909d
Magnus Larsen <golgothasTerror101@gmail.com>
2023-02-05 06:32:03 -0800

    char select, turnips 2 and 5 color

--- CharSelect/Selected.gd
+++ CharSelect/Selected.gd
@@ -3,8 +3,8 @@ extends Sprite
 func _process(delta):
 	match Global.character:
 		Global.Character.Carrot:
-			self.position.x = 291
-			self.position.y = 562
+			self.position.x = 251
+			self.position.y = 662
 		Global.Character.Potato:
 			self.position.x = 869
 			self.position.y = 762
