375b071b3cda472bb6f670b74bca288abf3e5e0b
NolanMeske <nolanmeske@icloud.com>
2023-02-05 12:48:42 -0800

    fix negatie floaters

--- Claw/Player.gd
+++ Claw/Player.gd
@@ -182,17 +182,12 @@ func transition_to(state):
 	match bob_state:
 		BobState.IDLE:
 			state_progress = 0
-		BobState.UP_GOTCHA:
-			if gotcha.is_garbage:
-				emit_signal("bitGarbage", $Mouth.get_global_position())
-			else:
-				emit_signal("bitTurnip", $Mouth.get_global_position())
-
+		BobState.UP_GOTCHA:		
+			emit_signal("bitTurnip", $Mouth.get_global_position())
 			gotcha.queue_free()
 			gotcha = null
 		BobState.UP_GARBAGE:
-
-			emit_signal("bitGarbage")
+			emit_signal("bitGarbage", $Mouth.get_global_position())
 			gotcha.queue_free()
 			gotcha = null
 
